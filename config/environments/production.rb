467da45fc851cc04afdbcd8fc1ae7a27fe6dcec1
Carlos Alonzo Lagos <calonzo@nicarao.agency>
2022-09-28 17:23:25 -0600

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.hosts << 'rails-production-e05d.up.railway.app'
+  config.hosts << ENV.fetch('CONFIG_HOST')
 end
